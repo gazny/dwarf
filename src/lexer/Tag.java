3d08b2f5538c6707fe62aad4e32ffb4de04d66b7
Mauricio Panelo JR <17281354+mpanelo@users.noreply.github.com>
2018-09-15 16:02:25 -0700

    created tags for every token

---
+++ src/lexer/Tag.java
@@ -0,0 +1,9 @@
+package lexer;
+
+public enum Tag {
+    IF, ELSE, AND, OR, TRUE, FALSE,
+    WHILE, FOR, BREAK,
+    NUMBER, IDENTIFIER, REAL, BASIC,
+    LE, GE, EQ, NE,
+    INDEX, MINUS, TEMP
+}

161178dfc0a92d40bb15a4e903b1437440ad5273
Mauricio Panelo JR <17281354+mpanelo@users.noreply.github.com>
2018-09-15 16:03:06 -0700

    added the start of execution

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import lexer.Tag;
+import lexer.Token;
+
+public class Main {
+    public static void main(String[] args) {
+        Token token = new Token(Tag.IF);
+        System.out.println("Hello, World!");
+    }
+}
